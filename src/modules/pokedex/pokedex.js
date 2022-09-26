7a629bffc144c0f4b37a2534fdea02f9c9321237
Blizzcane <blizzcane@gmail.com>
2022-09-25 20:12:41 -0500

    added bootstrap

--- src/modules/pokedex/pokedex.js
+++ src/modules/pokedex/pokedex.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Card from "react-bootstrap/Card";
 
-const Pokedex = ({pokemon, imgSource}) => {
+const Pokedex = ({ pokemon, imgSource }) => {
+  const [species, setSpecies] = useState(null);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    fetch(pokemon?.species.url, {
+      signal: abortController.signal,
+    })
+      .then((response) => response.json())
+      .then((data) => setSpecies(data))
+      .catch((error) => console.log(error.message));
+
+    return () => {
+      abortController.abort();
+    };
+  }, [pokemon]);
+
   return (
-    <div
-      className="container"
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <img src={imgSource} className="hero-image" width="30%" />
-      <div className="info">
-        <p>{pokemon?.name}</p>
-        <Pokedex />
-      </div>
+    <div>
+      <img src={imgSource} width="30%" />
+      <Card style={{ width: "25rem" }}>
+        <Card.Body>
+          <Card.Title>
+            {pokemon?.name.charAt(0).toUpperCase() + pokemon?.name.slice(1)}
+          </Card.Title>
+          <Card.Text>{species?.flavor_text_entries[0].flavor_text}</Card.Text>
+        </Card.Body>
+      </Card>
     </div>
   );
 };
