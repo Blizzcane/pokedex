bcf2144283741560dcb56936671323bab7d658a1
Blizzcane <blizzcane@gmail.com>
2022-10-02 22:00:15 -0500

    adding styling to info cards

--- src/common/components/PokemonCard.js
+++ src/common/components/PokemonCard.js
@@ -1,17 +1,25 @@
 import React from "react";
-import { Button, Card } from "react-bootstrap"; 
+import { Button, Card } from "react-bootstrap";
 
-const PokemonCard = ({ pokemon }) => { 
+const PokemonCard = ({ pokemon }) => {
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={pokemon.sprites.other.home.front_default} />
-      <Card.Body>
-        <Card.Title>{pokemon.name[0].toUpperCase() + pokemon.name.substring(1)}</Card.Title>
+    <Card style={{ width: "18rem" , minWidth: "16rem", margin: "10px" , flexBasis: "auto"}}>
+      <Card.Img
+        style={{ transform: "translate(0px, 20px)" }}
+        variant="top"
+        src={pokemon.sprites.other.home.front_default}
+      />
+      <Card.Body style={{ backgroundColor: "blue" }}>
+        <Card.Title>
+          {pokemon.name[0].toUpperCase() + pokemon.name.substring(1)}
+        </Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          <ul>
+            <li>#{pokemon.id}</li>
+            <li>Height: {pokemon.height}</li>
+            <li>Weight: {pokemon.weight}</li>
+          </ul>
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
   );
--- src/common/components/PokemonList.js
+++ src/common/components/PokemonList.js
@@ -3,11 +3,11 @@ import PokemonCard from "./PokemonCard";
 
 const PokemonList = ({ pokemon }) => {
   return (
-    <>
+    <div style={{display: "flex", flexDirection: "row", flexWrap: "wrap"}}>
       {pokemon.map((pokemon) => (
         <PokemonCard pokemon={pokemon} />
       ))}
-    </>
+    </div>
   );
 };
 export default PokemonList;
